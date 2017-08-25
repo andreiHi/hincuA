9a34090daa8d3d1aa538a471ba3bb0b37f8667c6
Andrei Hincu <andreih1981@gmail.com>
2017-08-25 07:59:55 +0300

    task4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j;
 
+/**
+ * Calculate.
+ *
+ * @author Hincu Andrei (andreih1981@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
 public class Calculate {
+	 /**
+     * Main.
+     * @param args - args.
+     */
     
     public static void main(String[] args) {
         System.out.println("Hello World");
