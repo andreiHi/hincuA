a6f880e31fa96c5ec7f97173cbe1d2b23b6e79a9
Andrei Hincu <andreih1981@gmail.com>
2017-08-31 13:33:26 +0300

    task 223

---
+++ chapter_001/src/main/java/ru/job4j/array/BubleSort.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+/**
+ * RotateArray.
+ * @author Hincu Andrei (andreih1981@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class BubleSort {
+    /**
+     * Сортировка пузырьком.
+     * @param array массив для сортировки.
+     * @return - отсортированный масив.
+     */
+    public int[] sort(int[] array) {
+        int[] a = array;
+        boolean end = false;
+        while (!end) {
+            end = true;
+            for (int i = 0; i < a.length - 1; i++) {
+                if (a[i] > a[i + 1]) {
+                    int temp = a[i];
+                    a[i] = a[i + 1];
+                    a[i + 1] = temp;
+                    end = false;
+                }
+            }
+        }
+        return a;
+    }
+}
--- chapter_001/src/main/java/ru/job4j/array/RotateArray.java
+++ chapter_001/src/main/java/ru/job4j/array/RotateArray.java
@@ -17,7 +17,7 @@ public class RotateArray {
         for (int i = 0; i < a.length / 2; i++) {
             for (int j = 0; j < a.length - 1 - i; j++) {
                 int temp = a[i][j];
-                a[i][j] = a[a.length - 1 -j][i];
+                a[i][j] = a[a.length - 1 - j][i];
                 a[a.length - 1 - j][i] = a[a.length - 1 - i][a.length - 1 - j];
                 a[a.length - 1 - i][a.length - 1 - j] = a[j][a.length - 1 - i];
                 a[j][a.length - 1 - i] = temp;
