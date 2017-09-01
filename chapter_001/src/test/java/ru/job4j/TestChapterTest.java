7bafee1909131758cb7ee20253f464007da6be72
andreiHi <andreih1981@gmail.com>
2017-09-01 11:34:37 +0300

    task 228

---
+++ chapter_001/src/test/java/ru/job4j/TestChapterTest.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Hincu Andrei (andreih1981@gmail.com)on 01.09.2017.
+ * @version $Id$.
+ * @since 0.1.
+ */
+public class TestChapterTest {
+    /**
+     * Test.
+     */
+    @Test
+    public void whenWordConteinsSubwordThenTrue() {
+        TestChapter testChapter = new TestChapter();
+        boolean rezultTest = testChapter.contains("Привет", "иве");
+        boolean ex = true;
+        assertThat(rezultTest, is(ex));
+    }
+}
