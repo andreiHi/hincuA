0f12742d8dfe7c3e88738ec2d09778d13ac09d77
Andrei Hincu <andreih1981@gmail.com>
2017-09-03 18:51:19 +0300

    task 6837

---
+++ chapter_002/src/test/java/ru/job4j/profession/ProfessionTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.profession;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Hincu Andrei (andreih1981@gmail.com)on 03.09.2017.
+ * @version $Id$.
+ * @since 0.1.
+ */
+public class ProfessionTest {
+    @Test
+    public void whenTeachesThenREsultString() {
+        Teacher teacher = new Teacher("Петров", 33, "Патологоанатомии");
+        Engineer engineer = new Engineer("Иванов", 25, "радиослесарь");
+        String testTeacing = teacher.teaches(engineer);
+        String ex = "Преподователь Пентров обучает Иванов курсу Патологоанатомии";
+        assertThat(testTeacing, is(ex));
+    }
+}
