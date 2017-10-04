1a9d965b45459e89008b42a5883c6d85753d3e27
Andrei Hincu <andreih1981@gmail.com>
2017-10-04 08:23:20 +0300

    Дополнительная задача по сортировке TreeMap

--- chapter_003/src/test/java/ru/job4j/litle/treemapsort/TreeMapSortTest.java
+++ chapter_003/src/test/java/ru/job4j/litle/treemapsort/TreeMapSortTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.TreeMap;
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 /**
  * @author Hincu Andrei (andreih1981@gmail.com)on 02.10.2017.
@@ -20,10 +22,14 @@ public class TreeMapSortTest {
     TreeMap<int[], Object> map = treeMapSort.sortByIncrease(new int[]{1, 2, 3});
             treeMapSort.sortByIncrease(new int[]{1, 2, 0});
             treeMapSort.sortByIncrease(new int[]{1, 2, 0, 1});
+            StringBuilder sb = new StringBuilder();
         for (Map.Entry<int[], Object> m : map.entrySet()) {
             int[] k = m.getKey();
-            System.out.println(Arrays.toString(k));
+            sb.append(Arrays.toString(k));
         }
+        StringBuilder ex = new StringBuilder();
+        ex.append("[1, 2, 0][1, 2, 0, 1][1, 2, 3]");
+        assertThat(ex.toString(), is(sb.toString()));
     }
 
     /**
@@ -35,10 +41,14 @@ public class TreeMapSortTest {
         TreeMap<int[], Object> map = treeMapSort.sortByWaning(new int[]{1, 2, 3});
         treeMapSort.sortByWaning(new int[]{1, 2, 0});
         treeMapSort.sortByWaning(new int[]{1, 2, 0, 1});
+        StringBuilder sb = new StringBuilder();
         for (Map.Entry<int[], Object> m : map.entrySet()) {
             int[] k = m.getKey();
-            System.out.println(Arrays.toString(k));
+            sb.append(Arrays.toString(k));
         }
+        StringBuilder ex = new StringBuilder();
+        ex.append("[1, 2, 3][1, 2, 0, 1][1, 2, 0]");
+        assertThat(ex.toString(), is(sb.toString()));
 
     }
 }
