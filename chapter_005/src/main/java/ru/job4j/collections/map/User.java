1f841068c4446c056db8df2c7e5e427fb3f47587
Andrei Hincu <andreih1981@gmail.com>
2017-10-15 09:04:43 +0300

    1. Создать модель User [#999]

---
+++ chapter_005/src/main/java/ru/job4j/collections/map/User.java
@@ -0,0 +1,25 @@
+package ru.job4j.collections.map;
+
+import java.util.Calendar;
+
+/**
+ * Модель User.
+ *
+ * @author Hincu Andrei (andreih1981@gmail.com) by 15.10.17;
+ * @version $Id$
+ * @since 0.1
+ */
+public class User {
+    /**
+     * Имя.
+     */
+    private String name;
+    /**
+     * Колличество детей.
+     */
+    private int children;
+    /**
+     * Дата рождения.
+     */
+    private Calendar birthday;
+}
