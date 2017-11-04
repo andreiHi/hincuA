d0bfa88eb82bd82bc2da37b761d7b1077e0d9f3b
Andrei Hincu <andreih1981@gmail.com>
2017-11-04 15:24:03 +0200

    testfrog

--- chapter_005/src/main/java/ru/job4j/collections/frogway/Pair.java
+++ chapter_005/src/main/java/ru/job4j/collections/frogway/Pair.java
@@ -8,14 +8,49 @@ package ru.job4j.collections.frogway;
  * @since 0.1
  */
 public class Pair {
-    int x;
-    int y;
-    int value;
+   private int x;
+   private int y;
+   private int value;
+
+    public Pair(int x, int y, int value) {
+        this.x = x;
+        this.y = y;
+        this.value = value;
+    }
+
+    private Pair prev;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Pair(int x, int y, int value, Pair prev) {
+        this.x = x;
+        this.y = y;
+        this.value = value;
+        this.prev = prev;
+    }
+
+    public Pair getPrev() {
+        return prev;
+
+    }
+
+    public void setPrev(Pair prev) {
+        this.prev = prev;
+    }
 
     public boolean equalsCords(Pair pair) {
         return this.x == pair.x && this.y == pair.y;
     }
     public boolean minValue (Pair pair) {
         return this.value  > pair.value;
+    }
+    public void increment() {
+        this.value = this.value + 1;
     }
 }
--- chapter_005/src/main/java/ru/job4j/collections/frogway/Way.java
+++ chapter_005/src/main/java/ru/job4j/collections/frogway/Way.java
@@ -38,10 +38,12 @@ public class Way {
     }
     public void start(int x, int y, int yF, int xF) {
         int[][] array = new int[10][16];
-        array[8 - 1][5 - 1] = -1;
-        array[9 - 1][14 - 1] = -1;
-        move(array, x - 1, y - 1,0, 0);
-        int n = array[10 - 1][9 - 1];
+        Pair[][] array2 = new Pair[10][16];
+        //array[8 - 1][5 - 1] = -1;
+       // array[9 - 1][14 - 1] = -1;
+     //   move(array, x - 1, y - 1,0, 0);
+        move2(array2, x - 1, y - 1,0, 0);
+        Pair n = array2[10 - 1][9 - 1];
         System.out.println(n);
         System.out.println(array.length);
         System.out.println(array[0].length);
@@ -51,43 +53,76 @@ public class Way {
             }
             System.out.println();
         }
-        for (int i = n-1; i > 1; i--) {
 
-            int xX = -3;
-            int yY = 0;
-            if (i == array[yF + yY][xF + xX]) {
-                yF = yF + yY;
-                xF = xF + xX;
-            }
-
-        }
-      //  StringBuilder sb = new StringBuilder();
-      //  moveRevers(9, 8, array, 0, 0, sb);
-       // System.out.println( sb);
+        StringBuilder sb = new StringBuilder();
+       // sb = result(9,8, array);
+        moveRevers(8, 9, array, 0, 0, sb);
+       System.out.println( sb);
     }
     public void  moveRevers( int x, int y, int [][]array, int xX, int yY, StringBuilder sb) {
         int steps = array[y][x];
-        steps = steps - 1;
-        y = y - yY;
-        x = x - xX;
+        y = y + yY;
+        x = x + xX;
+        if (x < 0) {
+            x = x + 16;
+        }
+        if (9 < y || y < 0) {
+            return;
+        }
+        int steps2 = steps - array[y][x];
+        if (steps == 1) {
+
+            return;
+        }
+        if (steps2 > 1 ) {
+            return;
+        }
+        if (steps2 < 0) {
+            return;
+        }
+
+        sb.append(y).append(":").append(x).append(" ");
+
+       moveRevers(x, y, array,-3, 0, sb);
+       moveRevers(x, y, array, -1, -2, sb);
+       moveRevers(x, y, array,-1, 2, sb);
+       moveRevers(x, y, array,-2, 1, sb);
+       moveRevers(x, y, array,-2, -1, sb);
+    }
+    public void move2(Pair[][] array, int x, int y, int xX, int yY) {
+        Pair pair = array[y][x];
+        if (pair == null) {
+            pair = new Pair(x, y,0);
+            array[y][x] = pair;
+        }
+        int n = pair.getValue();
+        x = x + xX;
+        y = y + yY;
         if (x > 15) {
             x = x - 16;
         }
         if (y > 9 || y < 0) {
             return;
         }
-        if (steps != array[y][x]) {
+        Pair pair1 = array[y][x];
+        if (pair1 == null) {
+            pair1 = new Pair(x, y, 0, pair);
+            array[y][x] = pair1;
+        }
+        if (pair1.getValue() == -1) {
             return;
         }
-        if (steps == 2) {
+        if (pair1.getValue() > 0 && pair1.getValue() < n) {
             return;
         }
-        sb.append(y).append(":").append(x).append(" ");
-       // steps = steps -1;
-       moveRevers(x, y, array,-3, 0, sb);
-       moveRevers(x, y, array,-1, -2, sb);
-       moveRevers(x, y, array,-1, 2, sb);
-       moveRevers(x, y, array,-2, 1, sb);
-       moveRevers(x, y, array,-2, -1, sb);
+        pair1.setValue(n + 1);
+        pair1.setPrev(pair);
+
+        move2(array, x, y, 3, 0);
+        move2(array, x, y, 1, -2);
+        move2(array, x, y, 1, 2);
+        move2(array, x, y, 2, 1);
+        move2(array, x, y, 2, -1);
+
     }
 }
