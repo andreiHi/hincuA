318de73fc5cbfd6a15861288d8c6b8a1a3f1f6bc
andreiHi <andreih1981@gmail.com>
2017-11-06 17:10:00 +0200

    2. Сделать ожидание вывода. [#1017]

--- chapter_005/src/main/java/ru/job4j/multithreading/threads/WordsAndSpaces.java
+++ chapter_005/src/main/java/ru/job4j/multithreading/threads/WordsAndSpaces.java
@@ -1,96 +1,176 @@
 package ru.job4j.multithreading.threads;
 
+import java.io.*;
+
 /**
  * @author Hincu Andrei (andreih1981@gmail.com)on 04.11.2017.
  * @version $Id$.
  * @since 0.1.
  */
 public class WordsAndSpaces implements Runnable {
-    private static String text = "— Eh bien, mon prince. Gênes et Lucques ne sont plus que des apanages, des поместья, de la famille Buonaparte. Non, je vous préviens que si vous ne me dites"
-            + " pas que nous avons la guerre, si vous vous permettez encore de pallier toutes les infamies, toutes les atrocités de cet Antichrist (ma parole, j'y crois) — je ne vous connais plus, vous "
-            + "n'êtes plus mon ami, vous n'êtes plus мой верный раб, comme vous dites 1. Ну, здравствуйте, здравствуйте. Je vois que je vous fais peur 2, садитесь и рассказывайте."
-            + "Так говорила в июле 1805 года известная Анна Павловна Шерер, фрейлина и приближенная императрицы Марии Феодоровны, встречая важного и чиновного князя Василия, первого приехавшего на ее вечер."
-            + " Анна Павловна кашляла несколько дней, у нее был грипп, как она говорила (грипп был тогда новое слово, употреблявшееся только редкими). В записочках, разосланных утром с красным лакеем, было"
-            + " написано без различия во всех:"
-            + "«Si vous n'avez rien de mieux à faire, Monsieur le comte (или mon prince), et si la perspective de passer la soirée chez une pauvre malade ne vous effraye pas trop,"
-            + "— Dieu, quelle virulente sortie! 4 — отвечал, нисколько не смутясь такою встречей, вошедший князь, в придворном, шитом мундире, в чулках, башмаках и звездах, с светлым выражением плоского лица."
-            +"— Eh bien, mon prince. Gênes et Lucques ne sont plus que des apanages, des поместья, de la famille Buonaparte. Non, je vous préviens que si vous ne me dites"
-            + " pas que nous avons la guerre, si vous vous permettez encore de pallier toutes les infamies, toutes les atrocités de cet Antichrist (ma parole, j'y crois) — je ne vous connais plus, vous "
-            + "n'êtes plus mon ami, vous n'êtes plus мой верный раб, comme vous dites 1. Ну, здравствуйте, здравствуйте. Je vois que je vous fais peur 2, садитесь и рассказывайте."
-            + "Так говорила в июле 1805 года известная Анна Павловна Шерер, фрейлина и приближенная императрицы Марии Феодоровны, встречая важного и чиновного князя Василия, первого приехавшего на ее вечер."
-            + " Анна Павловна кашляла несколько дней, у нее был грипп, как она говорила (грипп был тогда новое слово, употреблявшееся только редкими). В записочках, разосланных утром с красным лакеем, было"
-            + " написано без различия во всех:"
-            + "«Si vous n'avez rien de mieux à faire, Monsieur le comte (или mon prince), et si la perspective de passer la soirée chez une pauvre malade ne vous effraye pas trop,"
-            + "— Dieu, quelle virulente sortie! 4 — отвечал, нисколько не смутясь такою встречей, вошедший князь, в придворном, шитом мундире, в чулках, башмаках и звездах, с светлым выражением плоского лица.";
+    /**
+     * Хранилище текста.
+     */
+    private static StringBuilder text = new StringBuilder();
 
     /**
-     *  Метод запускает два потока.
-     * @throws InterruptedException
+     * Читаем файл и сохраняем в хранилище.
+     * @return текст.
+     * @throws IOException ошибка чтения.
      */
-    public void wordsAndSpaces() throws InterruptedException {
+    public StringBuilder readText() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(new File("C://projects//hincuA//chapter_005//king")));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        while (reader.ready()) {
+            sb.append(reader.readLine()).append(System.lineSeparator());
+        }
+        return sb;
+    }
+    /**
+     *  Метод запускает два потока.
+     * @throws InterruptedException ошибка прерывания.
+     */
+    public void wordsAndSpaces() throws IOException {
+        text = readText();
         System.out.println("Начало работы метода");
-        String[] words = text.split(" ");
-        char[] chars = text.toCharArray();
+        String[] word = text.toString().split(" ");
+        char[] chars = text.toString().toCharArray();
+        Thread countWords = new Thread() {
+            int count = 0;
+            @Override
+            public void run() {
+                try {
+                    if (!isInterrupted()) {
+                        System.out.println("Считаем слова.");
+                        for (String s : word) {
+                            count++;
+                        }
+                        System.out.println(String.format("Слов %s", count));
+                    } else {
+                        throw new InterruptedException();
+                    }
+                } catch (InterruptedException e) {
+                    System.out.println("Расчеты колличества слов прерваны.");;
+                }
+            }
+        };
+        Thread countSpaces = new Thread() {
+            int count = 0;
+            @Override
+            public void run() {
+                try {
+                    if (!isInterrupted()) {
+
+                        System.out.println("Считаем пробелы.");
+                        for (Character c : chars) {
+                            if (Character.isWhitespace(c)) {
+                                count++;
+                            }
+                        }
+                        System.out.println(String.format("Пробелов %s", count));
+                    } else {
+                        throw new InterruptedException();
+                    }
+                } catch (InterruptedException e) {
+                    System.out.println("Расчеты колличества пробелов прерваны");
+                }
+            }
+        };
+
+
+        try {
+            countSpaces.start();
+            countSpaces.join();
+            countWords.start();
+            countWords.join();
+            Thread.sleep(1000);
+            countSpaces.interrupt();
+            countWords.interrupt();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Завершение работы метода.");
+    }
+
+    public static void main(String[] args) throws IOException {
+        WordsAndSpaces word = new WordsAndSpaces();
+        System.out.println("Начало работы метода");
+        text = word.readText();
+        String[] words = text.toString().split(" ");
         Thread thread = new Thread() {
             int count = 0;
             @Override
             public void run() {
-                System.out.println("Считаем слова.");
-                for (String s : words) {
-                    count++;
+                try {
+                    if (!interrupted()) {
+                        System.out.println("Считаем слова.");
+                        for (String s : words) {
+                            count++;
+                        }
+                        System.out.println(String.format("Слов %s", count));
+                    } else {
+                        throw new InterruptedException();
+                    }
+                } catch (InterruptedException e) {
+                    System.out.println("Расчеты колличества слов прерваны.");
                 }
-                System.out.println(String.format("Слов %s", count));
+
             }
         };
         thread.start();
-        thread.join();
-        Thread spaces = new Thread() {
-            int count = 0;
-            @Override
-            public void run() {
+        Thread spaces = new Thread(word);
+        spaces.start();
+        try {
+            thread.join();
+            spaces.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread.interrupt();
+        spaces.interrupt();
+
+        System.out.println("Завершение метода.");
+
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            if (!Thread.interrupted()) {
+                int count = 0;
+                char[] chars = text.toString().toCharArray();
                 System.out.println("Считаем пробелы.");
                 for (Character c : chars) {
+                    if (Thread.interrupted()) {
+                        break;
+                    }
                     if (Character.isWhitespace(c)) {
                         count++;
                     }
                 }
                 System.out.println(String.format("Пробелов %s", count));
+            } else {
+                throw new  InterruptedException();
             }
-        };
-        spaces.start();
-        spaces.join();
-    }
-
-    public static void main(String[] args) {
-        WordsAndSpaces word = new WordsAndSpaces();
-        System.out.println("Начало работы метода");
-        String[] words = text.split(" ");
-        new Thread() {
-            int count = 0;
-            @Override
-            public void run() {
-                System.out.println("Считаем слова.");
-                for (String s : words) {
-                    count++;
-                }
-                System.out.println(String.format("Слов %s", count));
-            }
-        }.start();
-        Thread spaces = new Thread(word);
-        spaces.start();
-    }
-
-    @Override
-    public void run() {
-        int count = 0;
-        char[]chars = text.toCharArray();
-        System.out.println("Считаем пробелы.");
-        for (Character c : chars) {
-            if (Character.isWhitespace(c)) {
-                count++;
-            }
+        } catch (InterruptedException e) {
+            System.out.println("Расчеты колличества пробелов прерваны");
         }
-        System.out.println(String.format("Пробелов %s", count));
+
     }
+
 }
 
