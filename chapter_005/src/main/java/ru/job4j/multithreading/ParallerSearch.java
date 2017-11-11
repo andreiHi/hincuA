bac937f03cd0b4597217dc4cd2fe1ec7218e7fdf
Andrei Hincu <andreih1981@gmail.com>
2017-11-11 17:50:03 +0200

    transfer

--- chapter_005/src/main/java/ru/job4j/multithreading/ParallerSearch.java
+++ chapter_005/src/main/java/ru/job4j/multithreading/ParallerSearch.java
@@ -1,7 +1,10 @@
 package ru.job4j.multithreading;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
 
 /**
  * @author Hincu Andrei (andreih1981@gmail.com)on 10.11.2017.
@@ -10,19 +13,42 @@ import java.util.List;
  */
 public class ParallerSearch {
     private List<String> result = new ArrayList<>();
+    private Queue<File> files = new ArrayDeque<>();
     private final String root;
     private final String text;
     private final List<String> exts;
-    Thread searchFiles = new Thread(new Runnable() {
+    private Thread searchFiles = new Thread(new Runnable() {
         @Override
         public void run() {
-
+            File file = new File(root);
+            search(file);
         }
     });
-    Thread searchText = new Thread(new Runnable() {
+    private Thread searchText = new Thread(new Runnable() {
         @Override
         public void run() {
 
+            while (searchFiles.isAlive() || !files.isEmpty()) {
+                File file = files.poll();
+                try {
+                    BufferedReader reader = new BufferedReader(new FileReader(file));
+                    boolean found = false;
+                    while (reader.ready()) {
+                        String s = reader.readLine();
+                        if (s.contains(text)) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (found) {
+                        result.add(file.getPath());
+                    }
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     });
 
@@ -33,6 +59,38 @@ public class ParallerSearch {
     }
 
     public static void main(String[] args) {
+        String root = "/home/andrei/cod";
+        String text = "hello world";
+        List<String> exts = Arrays.asList("txt","log");
+        ParallerSearch parallerSearch = new ParallerSearch(root, text, exts);
+        parallerSearch.searchFiles.start();
+        parallerSearch.searchText.start();
+        while (parallerSearch.searchText.isAlive()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(parallerSearch.result);
+    }
 
+    private void search(File file) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            for (File f : files) {
+                if (f.isDirectory()) {
+                    search(f);
+                } else if (f.isFile()) {
+                    String name = f.getName();
+                    if (name.contains("\\.")) {
+                        name = name.split("\\.")[1];
+                        if (exts.contains(name)) {
+                            this.files.add(f);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
