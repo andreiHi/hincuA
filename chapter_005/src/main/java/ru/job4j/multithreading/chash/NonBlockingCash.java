2aa3d568c9bbdcbf4b327f0286a4bed0e26cafc7
Andrei Hincu <andreih1981@gmail.com>
2017-11-15 17:11:52 +0200

    transfer

---
+++ chapter_005/src/main/java/ru/job4j/multithreading/chash/NonBlockingCash.java
@@ -0,0 +1,15 @@
+package ru.job4j.multithreading.chash;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * .
+ *
+ * @author Hincu Andrei (andreih1981@gmail.com) by 15.11.17;
+ * @version $Id$
+ * @since 0.1
+ */
+public class NonBlockingCash {
+  //  private ConcurrentHashMap<User,Integer> map = new ConcurrentHashMap();
+
+}
---
+++ chapter_005/src/main/java/ru/job4j/multithreading/chash/User.java
@@ -0,0 +1,14 @@
+package ru.job4j.multithreading.chash;
+
+/**
+ * .
+ *
+ * @author Hincu Andrei (andreih1981@gmail.com) by 15.11.17;
+ * @version $Id$
+ * @since 0.1
+ */
+public class User {
+    String name;
+    int version = 0;
+
+}
