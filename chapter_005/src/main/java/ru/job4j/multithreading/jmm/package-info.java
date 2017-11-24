f1fe578fd0eb1a8f302d7be0d97ce6a3975cb59c
andreiHi <andreih1981@gmail.com>
2017-11-24 20:23:13 +0200

    transfer

--- chapter_005/src/main/java/ru/job4j/multithreading/jmm/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * .
- *
- * @author Hincu Andrei (andreih1981@gmail.com) by 07.11.17;
- * @version $Id$
- * @since 0.1
- */
-package ru.job4j.multithreading.jmm;
---
+++ chapter_007/src/main/java/ru/job4j/multithreading/poducer/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author Hincu Andrei (andreih1981@gmail.com)on 12.11.2017.
+ * @version $Id$.
+ * @since 0.1.
+ */
+package main.java.ru.job4j.multithreading.poducer;
