6e8a38845bcd21d46220a556f877abd432cc86ae
andreiHi <andreih1981@gmail.com>
2017-11-24 21:55:48 +0200

    transfer

---
+++ chapter_008/src/main/java/ru/job4j/uml/Category.java
@@ -0,0 +1,12 @@
+package ru.job4j.uml;
+
+/**
+ * @author Hincu Andrei (andreih1981@gmail.com)on 24.11.2017.
+ * @version $Id$.
+ * @since 0.1.
+ */
+public class Category {
+    private String max;
+    private String medium;
+    private String low;
+}
---
+++ chapter_008/src/main/java/ru/job4j/uml/Order.java
@@ -0,0 +1,17 @@
+package ru.job4j.uml;
+
+import java.io.File;
+
+/**
+ * @author Hincu Andrei (andreih1981@gmail.com)on 24.11.2017.
+ * @version $Id$.
+ * @since 0.1.
+ */
+public class Order {
+    private User user;
+    private int id;
+    String[] comments;
+    File[] files;
+    String state;
+    Category category;
+ }
