1ee597a5e394f3479977d4df67c471eccbbdacfe
Andrei Hincu <andreih1981@gmail.com>
2017-12-19 17:18:50 +0200

    transfer

---
+++ chapter_001/src/main/java/ru/job4j/Test1.java
@@ -0,0 +1,23 @@
+package ru.job4j;
+
+/**
+ * .
+ *
+ * @author Hincu Andrei (andreih1981@gmail.com) by 19.12.17;
+ * @version $Id$
+ * @since 0.1
+ */
+public class Test1 {
+
+    public int test() {
+        return 10;
+
+    }
+
+    public static void main(String[] args) {
+        Test1 t = new Test2();
+        t.test();
+
+    }
+
+}
