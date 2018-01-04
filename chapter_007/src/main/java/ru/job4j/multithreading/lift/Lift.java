eade3d7b5bf62a22093cfb3530b8e548ca212bc0
andreiHi <andreih1981@gmail.com>
2018-01-04 22:52:14 +0200

    lift task

---
+++ chapter_007/src/main/java/ru/job4j/multithreading/lift/Lift.java
@@ -0,0 +1,18 @@
+package ru.job4j.multithreading.lift;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+/**
+ * @author Hincu Andrei (andreih1981@gmail.com)on 04.01.2018.
+ * @version $Id$.
+ * @since 0.1.
+ */
+public class Lift implements Runnable{
+    private static final Logger LOG = LogManager.getLogger(Lift.class);
+
+    @Override
+    public void run() {
+
+    }
+}
