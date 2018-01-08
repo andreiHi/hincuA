91979d745ce8765aa1ef41ff386eda62d07fd094
andreiHi <andreih1981@gmail.com>
2018-01-08 19:05:12 +0200

    Echo Servlet

---
+++ chapter_009/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -0,0 +1,38 @@
+package ru.job4j.servlets;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @author Hincu Andrei (andreih1981@gmail.com)on 06.01.2018.
+ * @version $Id$.
+ * @since 0.1.
+ */
+public class EchoServlet extends HttpServlet {
+    private static final Logger LOG = LogManager.getLogger(EchoServlet.class);
+    List<String> users = new CopyOnWriteArrayList<String>();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.append("hello world." + this.users);
+        writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        this.users.add(req.getParameter("login"));
+        doGet(req, resp);
+    }
+}
