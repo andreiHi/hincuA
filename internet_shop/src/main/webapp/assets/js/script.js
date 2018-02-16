628f711c1b0eeb268b61b98097e5af72f6b6f4c1
Andrei Hincu <andreih1981@gmail.com>
2018-02-16 14:38:34 +0200

    transfer

--- internet_shop/src/main/webapp/assets/js/script.js
+++ internet_shop/src/main/webapp/assets/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
      $('#block-category').load("assets/include/block-category.html");
      $('#block-parameter').load("assets/include/block-parameter.html");
      $('#block-news').load("assets/include/block-news.html");
+     $('#block-footer').load("assets/include/block-footer.html");
 
      $('#newsticker').jCarouselLite({
          vertical:true,
