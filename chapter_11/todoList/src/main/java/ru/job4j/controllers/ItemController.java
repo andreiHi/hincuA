2ef03add62c53dddc84d991e0233907823d364e5
Andrei Hincu <andreih1981@gmail.com>
2018-03-08 13:39:48 +0200

    todo list

---
+++ chapter_11/todoList/src/main/java/ru/job4j/controllers/ItemController.java
@@ -0,0 +1,22 @@
+package ru.job4j.controllers;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * .
+ *
+ * @author Hincu Andrei (andreih1981@gmail.com) by 08.03.18;
+ * @version $Id$
+ * @since 0.1
+ */
+public class ItemController extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
---
+++ chapter_11/todoList/src/main/java/ru/job4j/model/Item.java
@@ -0,0 +1,54 @@
+package ru.job4j.model;
+
+import java.sql.Timestamp;
+
+/**
+ * .
+ *
+ * @author Hincu Andrei (andreih1981@gmail.com) by 08.03.18;
+ * @version $Id$
+ * @since 0.1
+ */
+public class Item {
+    private int id;
+    private String description;
+    private Timestamp created;
+
+    public Item() {
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    private boolean done;
+}
