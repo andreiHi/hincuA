072cbfebd064035ae35176282eb6cba1184fd481
andreiHi <andreih1981@gmail.com>
2018-04-02 23:40:20 +0300

    shop

--- chapter_11/shopCar/src/main/java/ru/job4j/actions/Process.java
+++ chapter_11/shopCar/src/main/java/ru/job4j/actions/Process.java
@@ -1,7 +1,9 @@
 package ru.job4j.actions;
 
+import com.google.gson.Gson;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
+import org.json.simple.JSONObject;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -12,6 +14,8 @@ import java.util.concurrent.ConcurrentHashMap;
  * @since 0.1.
  */
 public class Process {
+    private Gson gson;
+    JSONObject jsonObject;
     private static final Logger LOG = LogManager.getLogger(Process.class);
     private static final Map<String, Action> ACTIONS = new ConcurrentHashMap<String, Action>() { {
         put("create", new CreateUser());
