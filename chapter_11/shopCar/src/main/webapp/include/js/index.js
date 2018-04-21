cc9efdef4369091fb8a173206f853e93e65440ea
andreiHi <andreih1981@gmail.com>
2018-04-21 23:44:26 +0300

    create html for adverts

--- chapter_11/shopCar/src/main/webapp/include/js/index.js
+++ chapter_11/shopCar/src/main/webapp/include/js/index.js
@@ -8,10 +8,33 @@ function getAdverts(data) {
         conditions.get = 'all';
     }
     ajax('allAds', conditions, function (respons) {
+        var ul = document.createElement("ul");
+        ul.setAttribute('id', 'block-ad-list');
         $.each(respons, function (k,v) {
-           var id = k;
-           var advert = v;
-        })
+            var id = k;
+            var advert = v;
+            var li = ul.appendChild(document.createElement('li'));
+            var div = li.appendChild(document.createElement('div'));
+            div.classList.add('block-images-list');
+            li = div.appendChild(document.createElement('img'));
+            li.setAttribute('src','img?name=' + advert["car"]["images"]["image"].path +'');
+            li.setAttribute('height','200px');
+            li.setAttribute('width','200px');
+            li  = div.appendChild(document.createElement('p'));
+            li.classList.add('style-title-list');
+            var a = document.createElement("a");
+            a.setAttribute('id', k);
+            a.setAttribute('href', 'index.html');
+            a.append(advert['car']['model'].name );
+            li.append(a);
+
+            // ul.append('<p class="style-title-list">');
+            // ul.append('<a id="'+ k +'" href="index.html">' + advert['car']['model'].name + '</a></p>');
+            // ul.append('<p class="style-price-list" >Price <strong>20000</strong> руб.</p>');
+            // ul.append('</li>')
+        });
+
+        $('#product-list').append(ul);
     })
 }
 // <ul id="block-ad-list">
